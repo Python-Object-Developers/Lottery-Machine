5ac8fc1c5e6a18632f0f7f7182ba859bbbccf72d
YBCA7 <www.yebeichen@qq.com>
2023-01-17 13:18:18 +0800

    Optimize.

--- Lottery-Machine/Form1.Designer.cs
+++ Lottery-Machine/Form1.Designer.cs
@@ -162,6 +162,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.mint);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Lottery Machine";
             this.ResumeLayout(false);
--- Lottery-Machine/Properties/AssemblyInfo.cs
+++ Lottery-Machine/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Lottery-Machine")]
+[assembly: AssemblyTitle("Lottery Machine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lottery-Machine")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("Lottery Machine")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,5 +18,5 @@ using System.Runtime.InteropServices;
 //请将此类型的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 [assembly: Guid("ac2c8837-e476-4a22-9be0-0b1a3db8c0e7")]
-[assembly: AssemblyVersion("3.0.0.1")]
-[assembly: AssemblyFileVersion("3.0.0.1")]
+[assembly: AssemblyVersion("3.0.0.2")]
+[assembly: AssemblyFileVersion("3.0.0.2")]
