49784c0fef2866d97ffa3b3127a600233200689d
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2023-01-28 21:52:54 +0800

    Update AssemblyInfo.cs

--- Lottery-Machine/Properties/AssemblyInfo.cs
+++ Lottery-Machine/Properties/AssemblyInfo.cs
@@ -1,9 +1,5 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
 [assembly: AssemblyTitle("Lottery Machine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -12,10 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2022-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 [assembly: Guid("ac2c8837-e476-4a22-9be0-0b1a3db8c0e7")]
 [assembly: AssemblyVersion("3.0.2.2")]
