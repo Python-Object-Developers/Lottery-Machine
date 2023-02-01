9f79bfd6a0e7940f22a23132c6622cfdb76bd987
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2023-02-01 11:44:47 +0800

    Update.

--- Lottery-Machine/Properties/AssemblyInfo.cs
+++ Lottery-Machine/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ac2c8837-e476-4a22-9be0-0b1a3db8c0e7")]
-[assembly: AssemblyVersion("3.0.2.4")]
-[assembly: AssemblyFileVersion("3.0.2.4")]
+[assembly: AssemblyVersion("3.0.2.5")]
+[assembly: AssemblyFileVersion("3.0.2.5")]
